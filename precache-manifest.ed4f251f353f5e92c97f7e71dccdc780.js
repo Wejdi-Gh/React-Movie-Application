5127e33c7e40bce342480e9d186f566dbd96325e
Wejdi <wejdigharbi@yahoo.fr>
2019-03-13 00:43:59 +0100

    Updates

---
+++ precache-manifest.ed4f251f353f5e92c97f7e71dccdc780.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "de2fb77eed1dc3e7eb0700135d022fac",
+    "url": "/React-Movie-Application/static/media/Film5.de2fb77e.jpg"
+  },
+  {
+    "revision": "8d7a6bf6afaf78e1eff8",
+    "url": "/React-Movie-Application/static/css/main.45c37ab3.chunk.css"
+  },
+  {
+    "revision": "dc28afd2aa2fe1015fb1",
+    "url": "/React-Movie-Application/static/js/runtime~main.dc28afd2.js"
+  },
+  {
+    "revision": "23b2b218d4f5dc476a99",
+    "url": "/React-Movie-Application/static/js/2.23b2b218.chunk.js"
+  },
+  {
+    "revision": "0c96cb97e6f67f31325adb4623cc980c",
+    "url": "/React-Movie-Application/static/media/Film1.0c96cb97.jpg"
+  },
+  {
+    "revision": "129f604413b75357537a3b7fef2937e4",
+    "url": "/React-Movie-Application/static/media/Film3.129f6044.jpg"
+  },
+  {
+    "revision": "6979a4ea6ecf44c5fe08045261b48278",
+    "url": "/React-Movie-Application/static/media/Film4.6979a4ea.jpg"
+  },
+  {
+    "revision": "8d7a6bf6afaf78e1eff8",
+    "url": "/React-Movie-Application/static/js/main.8d7a6bf6.chunk.js"
+  },
+  {
+    "revision": "8915a463798eea03a3d7272ab19718ba",
+    "url": "/React-Movie-Application/static/media/Film6.8915a463.jpg"
+  },
+  {
+    "revision": "04baca55182c24265d6bcdebedc4c516",
+    "url": "/React-Movie-Application/static/media/Film7.04baca55.jpg"
+  },
+  {
+    "revision": "fe227a5276345a8305747e118a4682f3",
+    "url": "/React-Movie-Application/static/media/Film8.fe227a52.jpg"
+  },
+  {
+    "revision": "b434982eaac8bc65119e2645dbaca80c",
+    "url": "/React-Movie-Application/static/media/Top Greatest Movie .b434982e.mp4"
+  },
+  {
+    "revision": "97d4292c80cfaf85fdd84df5708c3d85",
+    "url": "/React-Movie-Application/static/media/bg-title-shop.97d4292c.png"
+  },
+  {
+    "revision": "2e366f970693c8f748a4dc07d7fb2127",
+    "url": "/React-Movie-Application/index.html"
+  }
+];
